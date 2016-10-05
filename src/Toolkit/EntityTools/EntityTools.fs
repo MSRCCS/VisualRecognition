5110ab8181c3e191ea1510ba70926f3eaa05d702
Yuxiao Hu <yuxiaohu@msn.com>
2016-10-05 16:34:29 -0700

    update satoriTool to make it more robust to different responses
    update satoriTool to make it more robust to different responses

--- src/Toolkit/EntityTools/EntityTools.fs
+++ src/Toolkit/EntityTools/EntityTools.fs
@@ -3,21 +3,60 @@
 open FSharp.Data
 open System
 
-type satoriRequest = JsonProvider<"https://www.bingapis.com/api/v5/search?knowledge=1&appId=D41D8CD98F00B204E9800998ECF8427E496F9910&responseformat=json&responsesize=m&q=tom%20cruise">
+type satoriRequest = JsonProvider<"https://www.bingapis.com/api/v5/search?knowledge=1&appId=D41D8CD98F00B204E9800998ECF8427E496F9910&responseformat=json&responsesize=m&q=space%20needle">
+type satoriRequestPlaces = JsonProvider<"https://www.bingapis.com/api/v5/search?knowledge=1&appId=D41D8CD98F00B204E9800998ECF8427E496F9910&responseformat=json&responsesize=m&q=paris%20hotel%20and%20casino">
 
 type Entitytools = 
-    static member getSatoriInfo entityName:string = 
+    static member getSatoriInfo entityName:string[] = 
+        let appID = "D41D8CD98F00B204E9800998ECF8427E496F9910"
+        
+        let rlt = [|"\t\t\t\t\t"|]
         try
             if not(String.IsNullOrWhiteSpace(entityName))  then
-                let satoriResponse = satoriRequest.Load("https://www.bingapis.com/api/v5/search?knowledge=1&appId=D41D8CD98F00B204E9800998ECF8427E496F9910&responseformat=json&responsesize=m&q=" + entityName)
-                //if String.length(entityName) > 0 && satoriResponse.Entities.Value.Length > 0 then
-                let satoriID = satoriResponse.Entities.Value.[0].Id
-                let satoriName = satoriResponse.Entities.Value.[0].Name
-                let satoriDesc = satoriResponse.Entities.Value.[0].Description
-                let satoriImage = satoriResponse.Entities.Value.[0].Image.ThumbnailUrl
-                satoriID.ToString() + "\t" + satoriName + "\t" + satoriDesc + "\t" + satoriImage
-            else
-                "\t\t\t"        
+                let request = "https://www.bingapis.com/api/v5/search?knowledge=1&appId=" + appID + "&responseformat=json&responsesize=m&q=" + entityName
+                let satoriRequestHtml = "https://www.bingapis.com/api/v5/search?knowledge=1&appId=" + appID + "&responseformat=html&responsesize=m&q=" + entityName
+                let satoriResponse = satoriRequest.Load(request)
+                
+                if satoriResponse.JsonValue.TryGetProperty("entities").IsSome 
+                      then satoriResponse.Entities.Value 
+                                |> Array.map (fun x ->
+                                let satoriID = x.Id
+                                let satoriName = x.Name
+                                let satoriDesc = x.Description
+                                let satoriType = 
+                                    if x.JsonValue.TryGetProperty("_type").IsSome 
+                                        then x.Type
+                                        //else x.EntityPresentationInfo.EntityTypeHints.[0] 
+                                        else if x.EntityPresentationInfo.JsonValue.TryGetProperty("entityTypeHints").IsSome
+                                            then x.EntityPresentationInfo.EntityTypeHints.[0] 
+                                            else x.EntityPresentationInfo.EntityTypeDisplayHint
+                                let satoriImage = x.Image.ThumbnailUrl
+                                let satoriSnippet = satoriRequestHtml + "&filters=sid:\"" + satoriID.ToString() + "\""
+                                satoriID.ToString() + "\t" + satoriName + "\t" + satoriDesc + "\t" + satoriImage + "\t" + satoriType + "\t" + satoriSnippet
+                                )
+                      else
+                          let satoriResponsePlaces = satoriRequestPlaces.Load(request)
+                          satoriResponsePlaces.Places.Value 
+                                |> Array.map (fun x ->
+                                let satoriID = x.Id
+                                let satoriName = x.Name
+                                let satoriDesc = x.Description
+                                let satoriType = 
+                                    if x.JsonValue.TryGetProperty("_type").IsSome 
+                                        then x.Type
+                                        else x.EntityPresentationInfo.EntityTypeHints.[0] 
+                                let satoriImage = 
+                                    if x.JsonValue.TryGetProperty("image").IsSome 
+                                        then x.Image.ContentUrl
+                                        elif x.JsonValue.TryGetProperty("photo").IsSome 
+                                            then x.Photo.[0].ContentUrl
+                                            else "ImageNotAvailable"
+                                let satoriSnippet = satoriRequestHtml + "&filters=sid:\"" + satoriID + "\""            
+                                satoriID.ToString() + "\t" + satoriName + "\t" + satoriDesc + "\t" + satoriImage + "\t" + satoriType + "\t" + satoriSnippet
+                                )
+            else          
+                rlt      
         with
         | :? System.Exception as ex -> 
-            Guid.Empty.ToString() + "\t\t\t"
+            printfn "!error! cannot get satori Info for %A\n" entityName
+            rlt 
--- src/Toolkit/TsvImage/FindSatoriID.cs
+++ src/Toolkit/TsvImage/FindSatoriID.cs
@@ -19,8 +19,14 @@ namespace TsvImage
             public string outTsv = "";
             [Argument(ArgumentType.Required, HelpText = "Column index for entity name string, start from 0")]
             public int dataCol = 0;
-            [Argument(ArgumentType.AtMostOnce, HelpText = "output extra detail info about the entity, if any")]
+            [Argument(ArgumentType.AtMostOnce, HelpText = "output detail info about the entity, if any, default=false")]
             public bool extraInfo = false;
+            [Argument(ArgumentType.AtMostOnce, HelpText = "concatenate the output, if the entry name matches multiple entities, default=false")]
+            public bool returnMultiple = false;
+            [Argument(ArgumentType.AtMostOnce, HelpText = "delimiter used to seperate multiple output strings, if returnMultipe==true, default is blank space")]
+            public string outDelimiter = " ";
+
+
         }
         static void FindSatoriID(ArgsFindSatoriID cmd)
         {
@@ -33,22 +39,58 @@ namespace TsvImage
 
             var dict = new Dictionary<string, string[]>(StringComparer.OrdinalIgnoreCase);
            
-            int count = 0;
-            int count_nonEmptyResult = 0;
-            int count_foundID = 0;
+            int count = 0;      //# of lines in the input file
+            int count_nonEmptyResult = 0;   //# of non-empty entity name strings which can be used to get satoriIDs
+            int count_foundID = 0;  //# of entities which really found at least one satoriID
+            int count_total_IDs = 0;  //# of total satoriIDs found ( note: 1. some entity cannot find satoriID 2. some entity may correspond to >1 satoriIDs 3. we didn't do dedup, although the same satoriID can be returned for different entity name;)
+            int count_notFoundID = 0;
 
-            var lines = File.ReadLines(cmd.inTsv)
+            var lines = File.ReadLines(cmd.inTsv, Encoding.UTF8)
                 //.AsParallel().AsOrdered()
                 .Select(line => line.Split('\t'))
                 .Select(cols =>
                 {
-                    Console.Write("lines processed: {0}\r", ++count);
-                    var oriLine = string.Join("\t", cols);
-                    string entityName = cols[cmd.dataCol].Trim().ToLower();
-                    string[] satoriInfo;
-                    if (!dict.ContainsKey(entityName))
-                    {
-                        satoriInfo = EntityTools.Entitytools.getSatoriInfo(entityName).Split('\t');
+                Console.Write("lines processed: {0}\r", ++count);
+                var oriLine = string.Join("\t", cols);
+                string entityName = cols[cmd.dataCol].Trim().ToLower().Replace("\"", "");
+                string[] satoriInfo = { };
+                if (!dict.ContainsKey(entityName))
+                {
+                        //normalize the entityName string to ascii
+                        byte[] tempBytes;
+                        tempBytes = System.Text.Encoding.GetEncoding("ISO-8859-8").GetBytes(entityName);
+                        string entityNameNormalized = System.Text.Encoding.UTF8.GetString(tempBytes);
+                        //Console.WriteLine("{0}\t{1}", entityName, entityNameNormalized);
+
+                        var satoriInfoAll = EntityTools.Entitytools.getSatoriInfo(entityNameNormalized)
+                                            .Select(ei => ei.Split('\t'))
+                                            //.Where(fields => fields[4] == "Attactions" || fields[4] == "Place")
+                                            .ToArray();
+
+                        if (!cmd.returnMultiple)
+                        {
+                            //only output the first matched entity
+                            satoriInfo = satoriInfoAll[0];
+                            //satoriInfo = EntityTools.Entitytools.getSatoriInfo(entityName)[0].Split('\t');
+                            count_total_IDs++;
+                        }
+                        else
+                        {
+                            satoriInfo = satoriInfoAll.Aggregate((working, next) =>
+                            {
+                                var aggregated = working;
+                                for (var i = 0; i < next.Length; i++)
+                                    aggregated[i] = working[i] + cmd.outDelimiter + next[i];
+                                return aggregated;
+                            });
+                            count_total_IDs += satoriInfoAll.Count();
+                        }
+
+                        if (satoriInfo[0].Length == 0)
+                            count_notFoundID++;
+
+                        Console.WriteLine("{0}\t{1}\t{2}", satoriInfo[0], satoriInfo[4], satoriInfo[1]);
+
                         dict.Add(entityName, satoriInfo);
                     }
                     else
@@ -57,17 +99,21 @@ namespace TsvImage
                     if (entityName.Length > 0)
                     {
                         count_nonEmptyResult++;
-                        if (satoriInfo[0] != Guid.Empty.ToString())
+                        if (satoriInfo.Length > 0 && satoriInfo[0] != Guid.Empty.ToString())
                             count_foundID++;
+                        
                     }
                     string satoriInfoStr = (cmd.extraInfo) ? string.Join("\t", satoriInfo) : satoriInfo[0];
-                    //Console.Write("EntityName={0}\tSatoriInfo={1}",entityName,satoriInfoStr);
                     return oriLine + "\t" + satoriInfoStr;
                 });
 
             string outTsv = (cmd.outTsv == "") ? Path.ChangeExtension(cmd.inTsv, "satori.tsv") : cmd.outTsv;
             File.WriteAllLines(outTsv, lines);
-            Console.WriteLine("\n# of Lines:\t{0}\n# of non-empty entity name:\t{1}\n# of satoriID found:\t{2}\n# of unique entities:\t{3}", count, count_nonEmptyResult, count_foundID, dict.Count());
+            Console.WriteLine("\n# of Lines:\t{0}\n# of non-empty entity names:\t{1}\n# of non-empty entity names found satoriIDs:\t{2}", count, count_nonEmptyResult, count_foundID);
+            Console.WriteLine("\n# of unique entities:\t{0}\n# of unique satoriIDs found\t{1}", dict.Count(), count_total_IDs);
+            Console.WriteLine("\n# of unique enities not found any satoriID\t{0}", count_notFoundID);
+
+
         }
     }
 }
